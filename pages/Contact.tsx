85f1baff127632863961d9488b2f83e45ccd3654
ArchRevolutionBD <mohammadrahat.study@gmail.com>
2025-12-05 02:09:33 +0600

    save changes

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -178,13 +178,15 @@ const Contact: React.FC = () => {
 
         {/* Map Placeholder */}
         <div className="mt-12 rounded-xl overflow-hidden shadow-lg h-96 bg-slate-200 relative">
-          <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.127863116568!2d90.42337731543183!3d23.77846198457551!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c79038202117%3A0x63390234f9273646!2sAftabnagar%2C%20Dhaka!5e0!3m2!1sen!2sbd!4v1634567890123!5m2!1sen!2sbd" 
-            width="100%" 
-            height="100%" 
-            style={{ border: 0 }} 
-            loading="lazy"
-            title="Office Location"
+          <iframe
+            src="https://maps.google.com/maps?q=Arch%20Revolution%20Dhaka&t=&z=15&ie=UTF8&iwloc=&output=embed"
+           width="100%"
+           height="100%"
+           style={{ border: 0 }}
+           allowFullScreen=""
+           loading="lazy"
+           referrerPolicy="no-referrer-when-downgrade"
+           title="Arch Revolution Office Location"
           ></iframe>
         </div>
       </div>
